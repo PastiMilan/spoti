e66499b9ad4692a44f1410f58d8811143ee7c16c
PastiMilan <71316469+PastiMilan@users.noreply.github.com>
2020-11-03 11:48:49 +0100

    Add files via upload
    Első feltöltés

---
+++ main.js
@@ -0,0 +1,63 @@
+let playpause=document.getElementById("play_pause");
+
+let play_music=false;
+let audio_player= document.getElementById("audioPlayer");
+
+
+function playMusic(){
+
+    if(!play_music){
+        play_music=true;
+        playpause.src="pause-circle-regular.svg";
+        audio_player.play();
+    }
+    else{
+        play_music=false;
+        playpause.src="play-solid.svg";
+        audio_player.pause();
+    }
+
+}
+
+function audioPlayer(){
+    audio_player.play();
+    let music_len=audio_player.duration;
+    console.log(music_len);
+
+
+    let min=0;
+    let sec=Math.round(music_len);
+    while(sec >= 60){
+        sec -= 60;
+        min += 1;
+    }
+
+
+
+    console.log(min+ ":" +sec);
+}
+function actualTime(){
+    let sec=Math.round(audio_player.currentTime);
+    timeline(sec);
+    let min=0;
+    while(sec >= 60){
+        sec -= 60;
+        min += 1;
+    }
+    
+    console.log(min+ ":" +sec);
+}
+function timeline(actual_sec){
+    let music_len=Math.round(audio_player.duration);
+    let percent= actual_sec/(music_len/100);
+    console.log(Math.round(percent)+"%");
+    document.getElementById("inner_line").style.width=percent+"%";
+}
+
+audioPlayer();
+setInterval(actualTime,1000);
+
+
+function changeMusic(){
+    audio_player.src="pestiest.mp3";
+}
